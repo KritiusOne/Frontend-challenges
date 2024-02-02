93262a131612dc49bcc436dfedf667fc72d46a8f
Julio Rodríguez <julio10489@gmail.com>
2024-02-01 21:27:24 -0500

    Adding: 02 Challenge, Succes

---
+++ 02-blogPreviewCard/tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			"yellow": "#f4d04e",
+			"grey": "#808080",
+			"minusBlack": "#121212",
+			"white": "#fff"
+		}
+	},
+	plugins: [],
+}
